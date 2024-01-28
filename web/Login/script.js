8273184cd2db5e445a3c91ccef9c2b990a6987c4
Ibrahim kocak <ibo66kocak@gmail.com>
2024-01-28 20:57:28 +0100

    grid and improvements

--- web/Login/script.js
+++ web/Login/script.js
@@ -4,12 +4,7 @@
 
 
 
-
-
-
-
-
-
+document.addEventListener("DOMContentLoaded",function AnzeigenButton(){
 
 
 
@@ -25,3 +20,65 @@ zeigPasswordButton.addEventListener("click", function() {
     zeigPasswordButton.textContent = "Anzeigen";
   }
 });
+
+});
+
+
+document.addEventListener("DOMContentLoaded",function(){
+
+  const LoginBox = document.getElementsByClassName('Login-con')[0];
+  const RegBox = document.getElementsByClassName("reg-con")[0];
+  const RegButton = document.getElementById('reg-button');
+  const LoginButton = document.getElementById('Login-button');
+
+
+
+  RegButton.addEventListener("click",function(){
+
+ 
+
+  
+  LoginBox.style.gridColumnStart  = "2";
+  LoginBox.style.gridColumnEnd  = "2";
+  LoginBox.style.gridRowStart = "1";
+  LoginBox.style.gridRowEnd = "1"; 
+
+  
+  RegBox.style.gridColumnStart = "1";
+  RegBox.style.gridColumnEnd  = "1";
+  RegBox.style.gridRowStart = "1";
+  RegBox.style.gridRowEnd = "1"; 
+
+
+})
+
+
+LoginButton.addEventListener("click",function(){
+
+
+  LoginBox.style.gridColumnStart  = "1";
+  LoginBox.style.gridColumnEnd  = "1";
+  LoginBox.style.gridRowStart = "1";
+  LoginBox.style.gridRowEnd = "1"; 
+
+  
+  RegBox.style.gridColumnStart = "2";
+  RegBox.style.gridColumnEnd  = "2";
+  RegBox.style.gridRowStart = "1";
+  RegBox.style.gridRowEnd = "1"; 
+
+})
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
