d43bde158270e254e85a1b6bd7f53b38c9712a49
Ibrahim kocak <48657333+Ibo66139@users.noreply.github.com>
2024-11-13 00:21:31 +0100

    Structured and portfolio added

---
+++ Portfolio/Home/script.js
@@ -0,0 +1,13 @@
+
+
+document.getElementsByClassName("Projekte-Con")[0].addEventListener("click", function() {
+    window.location.href = "http://127.0.0.1:5500/Portfolio/Home/my%20projects/index.html";  
+});
+
+
+document.getElementsByClassName("Person-Con")[0].addEventListener("click", function() {
+    window.location.href = "http://127.0.0.1:5500/Portfolio/Home/Login/index.html";  
+});
+
+
+
